2849d61cc562813195d3c0cd877da826a83157a0
jose-115 <joseveiga1234@gmail.com>
2023-08-23 12:42:07 -0400

    feat: :sparkles: Adicionando métodos get e post

--- dist/server.js
+++ dist/server.js
@@ -7,7 +7,20 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const PORT = 3000;
 var horaAtual = new Date().toLocaleTimeString();
+//VISUALIZAR NO NAVEGADOR É O METODO GET
 app.get('/hora', function (req, res) {
-    res.send(`agora são ${horaAtual}`);
+    res.json(`agora são ${horaAtual}`);
 });
+//POST
+app.post('/soma', (req, res) => {
+    //const {val1,val2} = req.params
+    //const {q1,q2} = req.query
+    const { v1, v2 } = req.body;
+    const soma = v1 + v2;
+    //res.json({soma})
+});
+//PUT
+app.put("");
+//DELETE
+app.delete("");
 app.listen(PORT, () => { console.log(`servidor rodando na porta = ${PORT}`); });
--- src/servers.ts
+++ src/servers.ts
@@ -1,13 +1,24 @@
 import express from "express";
-
 const app = express();
-
 const PORT = 3000;
 
 var horaAtual = new Date().toLocaleTimeString();
-
+//GET para ver no navegador
 app.get('/hora',function(req,res){
-    res.send(`agora são ${horaAtual}`);
+    res.json(`agora são ${horaAtual}`);
 })
+
+//POST
+app.post('/soma', (req,res)=>{
+    //const {val1,val2} = req.params
+    //const {q1,q2} = req.query
+    const {v1,v2} = req.body
+    const soma = v1+v2
+    //res.json({soma})
+});
+//PUT
+app.put("")
+//DELETE
+app.delete("")
 
 app.listen(PORT, () => {console.log(`servidor rodando na porta = ${PORT}`)})
